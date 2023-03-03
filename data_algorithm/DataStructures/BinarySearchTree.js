89c7b9d742d514d1a47efb96d82723456423d2be
FardilIqbal <iqbalfardil@gmail.com>
2023-03-03 12:29:42 -0700

    Added Binary Search

---
+++ data_algorithm/DataStructures/BinarySearchTree.js
@@ -0,0 +1,85 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+
+    isEmpty(){
+        return this.root === null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+
+    insertNode(root, newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            } else{
+                this.insertNode(root.left, newNode)
+            }
+        } else {
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+    search(root,value){
+        if(!root){
+            return false
+        } else {
+            if(root.value === value){
+                return true
+            } else if(value<root.value){
+               return this.search(root.left, value) 
+            } else{
+                return this.search(root.right,value)
+            }
+        }
+    }
+    //Depth Firsth Search
+    preOrder(root){
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value)
+            this.inOrder(root.right)
+        }
+    }
+    min(root){
+        if(!root.left){
+            return root.value
+        }else{
+            return this.min(root.left)
+        }
+    }
+    max(root){
+        if(!root.right){
+            return root.value
+        }
+        else{
+            return this.max(root.right)
+        }
+    }
+}
