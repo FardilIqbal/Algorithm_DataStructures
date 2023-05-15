b03e024a8c8d098311337fe23b5e84a8f026bbdf
FardilIqbal <iqbalfardil@gmail.com>
2023-05-14 21:19:37 -0600

    Very Cool

--- data_algorithm/Algorithms/QuickSort.js
+++ data_algorithm/Algorithms/QuickSort.js
@@ -1,20 +1,53 @@
-const quickSort = (arr) =>{
-    if(arr.length<2){
-        return arr
+//Time Complexity: O(n*log(n))
+
+
+// const quickSort = (arr) =>{
+//     if(arr.length<2){
+//         return arr
+//     }
+//     let pivot = arr[arr.length-1]//pivot = -6
+//     let left = []
+//     let right = []
+//     for (let i = 0; i<arr.length-1;i++){
+//         if(arr[i] < pivot){
+//             left.push(arr[i])//FIRST ITERATION OF quickSort: []
+//         }else{
+//             right.push(arr[i])//FIRST ITERATION OF quickSort: [8,20,-2,4]
+//         }
+//     }
+//     return[...quickSort(left),pivot,...quickSort(right)]
+    
+// }
+
+// const arr = [8,20,-2,4,-6]
+
+// console.log(quickSort(arr))
+
+function quickSort(arr,left=0,right=arr.length-1){
+    if(left<right){
+        let pivotIndex = pivot(arr,left,right)
+        quickSort(arr,left,pivotIndex-1)
+        quickSort(arr,pivotIndex+1,right)
     }
-    let pivot = arr[arr.length-1]
-    let left = []
-    let right = []
-    for (let i = 0; i<arr.length-1;i++){
-        if(arr[i]<pivot){
-            left.push(arr[i])
-        }else{
-            right.push(arr[i])
-        }
-    }
-    return[...quickSort(left),pivot,...quickSort(right)]
+    return arr;
+
+
+
 }
 
-const arr = [8,20,-2,4,-6]
-
-console.log(quickSort(arr))
+function pivot(arr, start=0,end=arr.length+1){
+    function swap(array,i,j){
+        var temp = array[i]
+        array[i]=array[j]
+        array[j] = temp
+    }
+    var pivot = arr[start]
+    var swapIdx = start
+    for(var i=start+1; i<arr.length; i++){
+        if(pivot>arr[i]){
+            swapIdx++
+            swap(arr,swapIdx,i)
+        }
+        swap(arr,start,swapIdx)
+    }
+}
